14dc8ab7463f1f24d3ef591245461ef7f8f99252
nietzche <havocf4u@gmail.com>
2020-05-14 16:48:00 +0200

    First web services implemented need to be tested out.

--- WebServerWeb/src/webservice/WebServiceLocal.java
+++ WebServerWeb/src/webservice/WebServiceLocal.java
@@ -9,8 +9,82 @@ import pojo.Local;
 public class WebServiceLocal {
     
 	@WebMethod
-	public String method1 (Local local) {
-		
-		return("Local " + local.getNomLocal() + " creat");
+	public void altaLocal(Local local, List<Accessibilitat> accessibilitat) throws ExceptionController {
+
+		try{
+			InitialContext cxt = new InitialContext();
+			if ( cxt != null ){
+				DataSource ds = (DataSource) cxt.lookup( "java:jboss/PostgreSQL/eAccessible");
+				if ( ds == null ) {
+					strEstat = "Error al crear el datasource";
+					throw new ErrorException(strEstat);
+				}
+				else{
+					connection = ds.getConnection();
+
+					String query = "insert into eAccessible.Local (codiTipoLocal, codiCarrer, nomCarrer, nomVia, numero, nomLocal, observacions, verificat) values('"+codiTipoLocal+"','"+codiCarrer+"','"+nomCarrer+"','"+nomVia+"','"+numero+"','"+nomLocal+"','"+observacions+"','"+verificat+"')");
+					Statement stm = connection.createStatement();
+					stm.executeUpdate(query);
+
+					for(int i=0; i<accessibilitat.size(); i=i+1) {
+						stm.executeUpdate("insert into eAccessible.accessibilitat (codiaccessibilitat,codilocal,codicaracteristica,valor,verificat) values('"+accessibilitat.get(i).getCodiaccessibilitat()+"','"+accessibilitat.get(i).getCodilocal()+"','"+accessibilitat.get(i).getCodicaracteristica()+"','"+accessibilitat.get(i).getValor()+"','"+accessibilitat.get(i).getVerificat()+"')");
+					}
+
+					connection.close();
+					stm.close();
+				}
+			}
+		}
+		catch(Exception e) {
+			strEstat = "Error amb la bdd";
+			throw new ErrorException(strEstat);
+
+		}
+		finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@WebMethod
+	public void baixaLocal(int codiLocal) throws ExceptionController {
+		String strEstat = new String();
+		Connection connection = null;
+
+		try {
+			InitialContext cxt = new InitialContext();
+			if (cxt != null) {
+				DataSource ds = (DataSource) cxt.lookup("java:jboss/PostgreSQL/eAccessible");
+				if (ds == null) {
+					strEstat = "Error al crear el datasource";
+					throw new ErrorException(strEstat);
+
+				} else {
+					connection = ds.getConnection();
+					String query = "delete from eAccessible.accessibilitat where codilocal=" + codiLocal;
+					Statement stm = connection.createStatement();
+					stm.executeUpdate(query);
+
+					query = "delete from eAccessible.local where codilocal=" + codiLocal;
+					stm.executeUpdate(query);
+
+					connection.close();
+					stm.close();
+				}
+			}
+		} catch (Exception e) {
+			strEstat = "Error amb la bdd";
+			throw new ErrorException(strEstat);
+
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
