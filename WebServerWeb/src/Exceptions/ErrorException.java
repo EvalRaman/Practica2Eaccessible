a26e2e8de4fd3dc5c4bde73745299f839d7b93ba
nietzche <havocf4u@gmail.com>
2020-05-15 20:33:30 +0200

    l

--- WebServerWeb/src/Exceptions/ErrorException.java
+++ WebServerWeb/src/Exceptions/ErrorException.java
@@ -1,4 +1,11 @@
+package Exceptions;
+
 public class ErrorException extends Exception {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -466366471595696474L;
+
     public ErrorException(String errorMessage) {
         super(errorMessage);
     }
